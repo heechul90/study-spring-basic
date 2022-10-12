de765c2610af1e59519e135b92a3fa5c3402a8cf
hclee <hclee@dataeum.kr>
2022-10-12 15:30:48 +0900

    findBean test - 동일한 타입이 둘 이상

--- src/test/java/study/spring/basic/core/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/study/spring/basic/core/beanfind/ApplicationContextInfoTest.java
@@ -3,20 +3,19 @@ package study.spring.basic.core.beanfind;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.spring.basic.AppConfig;
 
 public class ApplicationContextInfoTest {
 
-    AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
     @DisplayName("모든 빈 출력하기")
     void findAllBean() {
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
-            Object bean = applicationContext.getBean(beanDefinitionName);
+            Object bean = ac.getBean(beanDefinitionName);
             System.out.println("name = " + beanDefinitionName + ", object = " + bean);
         }
     }
@@ -24,14 +23,14 @@ public class ApplicationContextInfoTest {
     @Test
     @DisplayName("애플리케이션 빈 출력하기")
     void findApplicationBean() {
-        String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
-            BeanDefinition beanDefinition = applicationContext.getBeanDefinition(beanDefinitionName);
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
             //Role ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
             //Role ROLE_INFRASTRUCTURE: 스프링이 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
-                Object bean = applicationContext.getBean(beanDefinitionName);
+                Object bean = ac.getBean(beanDefinitionName);
                 System.out.println("name = " + beanDefinitionName + ", object = " + bean);
             }
         }
