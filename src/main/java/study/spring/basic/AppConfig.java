bc0b502fc310cc37f36aefa0b66db557c0995b97
hclee <hclee@dataeum.kr>
2022-10-12 14:50:58 +0900

    appConfig refactoring

--- src/main/java/study/spring/basic/AppConfig.java
+++ src/main/java/study/spring/basic/AppConfig.java
@@ -1,6 +1,8 @@
 package study.spring.basic;
 
+import study.spring.basic.core.discount.DiscountPolicy;
 import study.spring.basic.core.discount.FixDiscountPolicy;
+import study.spring.basic.core.repository.MemberRepository;
 import study.spring.basic.core.repository.MemoryMemberRepository;
 import study.spring.basic.core.service.MemberService;
 import study.spring.basic.core.service.MemberServiceImpl;
@@ -10,10 +12,18 @@ import study.spring.basic.core.service.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
