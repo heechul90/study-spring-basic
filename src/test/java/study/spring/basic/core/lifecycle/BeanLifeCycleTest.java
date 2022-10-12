89b8707840dead429c5174e9a6152531b2a8e3ac
hclee <hclee@dataeum.kr>
2022-10-12 17:47:29 +0900

    빈 생명주기 콜백 시작

---
+++ src/test/java/study/spring/basic/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,27 @@
+package study.spring.basic.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleconfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close(); //스프링 컨테이너를 종료, ConfigureableApplicationContext 필요
+    }
+
+    @Configuration
+    static class LifeCycleconfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
